c860ad8c4b25749804aa4f463172a6a74314ece9
Sanjay Git Learning <tosanjaysaraf@gmail.com>
2019-04-28 10:17:34 +0530

    Merge Rebase -1

--- calc.py
+++ calc.py
@@ -2,16 +2,6 @@ def add(x,y):
     pass
 def subtract(x,y):
     pass
-def multiply(x,y):
-    pass
-def divide(x,y):
-    pass
-def square(x,y):
-    pass
-
-def multiply(x,y):
-    return x*y
-
 def divide(x,y):
     return x-y
 
